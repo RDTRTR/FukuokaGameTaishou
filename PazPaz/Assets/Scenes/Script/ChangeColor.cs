d4799fcb9836b5bb735e5ad39638dcf23e702ddb
AnjuzzZZ <56670053+AnjuzzZZ@users.noreply.github.com>
2021-12-09 12:29:46 +0900

    スクリプト正常化

--- PazPaz/Assets/Scenes/Script/ChangeColor.cs
+++ PazPaz/Assets/Scenes/Script/ChangeColor.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
-
 public class ChangeColor : MonoBehaviour
 {
     // Unityエディタ上で変えたい色を指定できる。
     public Color color = Color.red;
     public GameObject plane;
+    int count = 0;
+    bool changeflag = false;
+    public panelsetcolor mycolor = panelsetcolor.White;
+
     //オブジェクトが衝突したとき
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "Player")
+        if (other.gameObject.name == "Blue")
         {
             var change = other.gameObject.GetComponent<Renderer>().material;
 
             plane.gameObject.GetComponent<Renderer>().material = change;
+
+            mycolor = panelsetcolor.Blue;
         }
+
+        if (other.gameObject.name == "Red")
+        {
+            var change = other.gameObject.GetComponent<Renderer>().material;
+
+            plane.gameObject.GetComponent<Renderer>().material = change;
+
+            mycolor = panelsetcolor.Red;
+        }
+
+        //Debug.Log(count);
     }
 }
--- PazPaz/Assets/Scenes/Script/CubeMove.cs
+++ PazPaz/Assets/Scenes/Script/CubeMove.cs
@@ -11,6 +11,10 @@ public enum panelsetcolor
 
 public class CubeMove : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 	public panelsetcolor winnercolor;
 
 	private bool Colorgudge()
@@ -37,44 +41,65 @@ public class CubeMove : MonoBehaviour
 
 	}
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 	Vector3 rotatePoint = Vector3.zero;  //回転の中心
 	Vector3 rotateAxis = Vector3.zero;   //回転軸
 	float cubeAngle = 0f;                //回転角度
 
 	public GameObject[] panelObject;
+<<<<<<< Updated upstream
 	float cubeSizeHalf;                  //キューブの大きさの半分
 	bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
 	bool latestisRotate = false;
 
+=======
+
+	float cubeSizeHalf;                  //キューブの大きさの半分
+	bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
+	bool latestisRotate = false;
+>>>>>>> Stashed changes
 
 	void Start()
 	{
-		cubeSizeHalf = transform.localScale.x / 2f;
-	}
-	
 
+
+		cubeSizeHalf = transform.localScale.x / 2f;
+
+<<<<<<< Updated upstream
 	void Update()
 	{
 		//回転中は入力を受け付けない
 		if (isRotate)
 		{
+=======
+	}
+	void Update()
+	{
+		//回転中は入力を受け付けない
+		if (isRotate) { 
+		
+>>>>>>> Stashed changes
 			latestisRotate = true;
 
 			return;
 		}
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 		if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
-			
 			rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
 			rotateAxis = new Vector3(0, 0, -1);
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
-			
 			rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
 			rotateAxis = new Vector3(0, 0, 1);
-			
 		}
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
@@ -87,55 +112,48 @@ public class CubeMove : MonoBehaviour
 			rotateAxis = new Vector3(-1, 0, 0);
 		}
 		// 入力がない時はコルーチンを呼び出さないようにする
-		if (rotatePoint == Vector3.zero)
+		if (rotatePoint == Vector3.zero){
 			return;
+		}
+
 		StartCoroutine(MoveCube());
+
+		if (latestisRotate)
+		{
+			if (Colorgudge())
+			{
+				Debug.Log("win");
+			}
+			latestisRotate = false;
+		}
+
 	}
+
 
 
 	IEnumerator MoveCube()
 	{
 		//回転中のフラグを立てる
 		isRotate = true;
-
 		//回転処理
 		float sumAngle = 0f; //angleの合計を保存
 		while (sumAngle < 90f)
 		{
 			cubeAngle = 0.5f; //ここを変えると回転速度が変わる
 			sumAngle += cubeAngle;
-
 			// 90度以上回転しないように値を制限
 			if (sumAngle > 90f)
 			{
 				cubeAngle -= sumAngle - 90f;
 			}
 			transform.RotateAround(rotatePoint, rotateAxis, cubeAngle);
-
 			yield return null;
 		}
-
 		//回転中のフラグを倒す
 		isRotate = false;
 		rotatePoint = Vector3.zero;
 		rotateAxis = Vector3.zero;
 
 		yield break;
-	}
-
-
-	bool ObjDic(Vector3 DirVec)
-    {
-		RaycastHit hit;
-
-		Physics.Raycast(transform.position, DirVec, out hit);
-		Debug.DrawRay(transform.position, DirVec);
-			
-		if(hit.distance <= 0.8)
-        {
-			return true;
-        }
-
-		return false;
 	}
 }
